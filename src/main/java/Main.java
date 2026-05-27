ed843df233380609e5b5dfb94752009b912d714a
PutilovSy <denisperel2005@gmail.com>
2026-05-27 13:11:50 +0500

    small correction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,36 +4,11 @@ import java.sql.SQLException;
 import javax.swing.SwingUtilities;
 
 public class Main {
-
-    private static final String DB_URL = "jdbc:sqlite:D:/Practice/DB/mini-banking";
-
     public static void main(String[] args) {
-        testConnection();
 
         SwingUtilities.invokeLater(() -> {
             LoginWindow window = new LoginWindow();
             window.setVisible(true);
         });
-    }
-
-    public static void testConnection() {
-        Connection connection = null;
-
-        try {
-            connection = DriverManager.getConnection(DB_URL);
-            System.out.println("Ура! Подключение к базе данных SQLite прошло успешно.");
-
-        } catch (SQLException e) {
-            System.out.println("Ошибка подключения: " + e.getMessage());
-
-        } finally {
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ex) {
-                System.out.println("Ошибка при закрытии соединения: " + ex.getMessage());
-            }
-        }
     }
 }
